51a215f1cd8ead25d618edd8c70eadf0960c275b
Candidimus <72453941+Candidimus@users.noreply.github.com>
2020-10-07 15:39:51 -0400

    a lotta testing stuff

---
+++ docs/scripts/scriptkid.js
@@ -0,0 +1,11 @@
+/* script stuff works how i think it does, remember to use the java force/
+
+
+/* can set up function definitions alla
+
+function name(variables)
+{
+	content
+}
+
+also set up definitions and logic statements/
